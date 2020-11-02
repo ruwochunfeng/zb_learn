e6ebfdc2bf0c30e572620ac91d68b2fdd716ec82
ruwochunfeng <2267136614@qq.com>
2020-11-02 21:44:18 +0800

    add Hanming Distance

---
+++ algorithm/hammingDistance.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int hammingDistance(int x, int y)
+{
+   int diff = x^y, ans = 0;
+   
+   while(diff)
+   {
+      ans += diff & 1;
+      diff >>=1;
+   }
+   
+   return ans;
+}
+
