7174b28f2ce10652bde250bb6c95abec26d1bf50
ruwochunfeng <2267136614@qq.com>
2020-11-03 21:48:29 +0800

    add reverse Bits

---
+++ algorithm/reverseBits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+unsigned int reverseBits(unsigned int n)
+{
+   unsigned ing ans = 0;
+   
+   for(int i = 0;i < 32;++i)
+   {
+       ans <<=1;
+       ans += n&1;
+       n >>=1;
+   
+   }
+   
+   return ans;
+
+}
