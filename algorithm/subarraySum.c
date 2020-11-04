97cd84c6fc59b968cd426a4576f820c9d958ceb4
ruwochunfeng <2267136614@qq.com>
2020-11-04 21:45:20 +0800

    add sub array sum

---
+++ algorithm/subarraySum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int subarraySum(vector<int>& nums, int k)
+{
+   int count = 0; psum = 0;
+   
+   unordered_map<int,int> hashmap;
+   
+   hashmap[0] = 1;
+   
+   for(int i :nums)
+   {
+      psum+=i;
+      count +=hashmap[psum-k];
+      ++hashmap[psum];
+   
+   }
+   
+   return count;
+}
