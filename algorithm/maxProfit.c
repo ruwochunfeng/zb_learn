2008f2982cb207e2d7eceac65501aa29f9cf86e8
ruwochunfeng <2267136614@qq.com>
2020-11-17 21:48:17 +0800

    add max profit

---
+++ algorithm/maxProfit.c
@@ -0,0 +1,13 @@
+int maxProfit(vector<int>& prices)
+{
+
+   int sell= 0, buy = INT_MIN;
+   
+   for(int i = 0;i  <  prices.size();  ++i)
+   {
+      buy = max(buy, -prices[i]);
+      sell = max(sell,  buy + prices[i]);
+   }
+
+   return  sell;
+}
